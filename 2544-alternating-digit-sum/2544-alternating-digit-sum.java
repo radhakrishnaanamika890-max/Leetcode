e57f8f10f2a9cc3025c82d947549a96f6131c8d6
Anamika R <radhakrishnaanamika890@gmail.com>
2026-07-22 21:06:11 +0530

    Time: 0 ms (100%), Space: 42.3 MB (13.23%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        int ans=0;
+        int sign=1;
+        while(n>0){
+            sign*=-1;
+            ans+=sign*(n%10);
+            n=n/10;
+        }return ans*sign;
+    }
+}
