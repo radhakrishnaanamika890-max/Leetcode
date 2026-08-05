a54958ca5963748c0aafe36d86b8e589c85efc61
Anamika R <radhakrishnaanamika890@gmail.com>
2026-08-05 22:14:40 +0530

    Time: 6 ms (99.23%), Space: 49.8 MB (44.25%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
