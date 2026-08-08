5cdcd627dfe153440eb4361d1a1051d7912f67b5
Anamika R <radhakrishnaanamika890@gmail.com>
2026-08-08 14:46:40 +0530

    Time: 3 ms (95.44%), Space: 46.3 MB (94.77%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+        StringBuilder result = new StringBuilder();
+        int cycle = 2 * numRows - 2;
+        for (int row = 0; row < numRows; row++) {
+            for (int i = row; i < s.length(); i += cycle) {
+                result.append(s.charAt(i));
+                int diagonal = i + cycle - 2 * row;
+                if (row != 0 &&
+                    row != numRows - 1 &&
+                    diagonal < s.length()) {
+                    result.append(s.charAt(diagonal));
+                }
+            }
+        }
+        return result.toString();
+    }
+}
