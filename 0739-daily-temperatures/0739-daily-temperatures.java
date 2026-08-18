3b4d47b04d6918069c4a42d0dbaac56d6a907156
Anamika R <radhakrishnaanamika890@gmail.com>
2026-08-18 20:29:38 +0530

    Time: 7 ms (99.5%), Space: 111.9 MB (7.37%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        int[] stack = new int[n]; 
+        int top = -1;                    
+        for (int i = 0; i < n; i++) {
+            while (top >= 0 && temperatures[stack[top]] < temperatures[i]) {
+                int prevIndex = stack[top--]; 
+                answer[prevIndex] = i - prevIndex;
+            }
+            stack[++top] = i; 
+        }     
+        return answer;
+    }
+}
