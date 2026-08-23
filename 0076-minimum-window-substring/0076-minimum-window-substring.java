845770bb2775beeed3d0a09a69cc5aa68216743a
Anamika R <radhakrishnaanamika890@gmail.com>
2026-08-23 20:50:32 +0530

    Time: 16 ms (55.68%), Space: 47.4 MB (38.72%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,20 +1,14 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public String minWindow(String s, String t) {
         Map<Character, Integer> map = new HashMap<>();
         for (char c : t.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
-
         int left = 0, right = 0, count = map.size(), minLen = Integer.MAX_VALUE, start = 0;
-
         while (right < s.length()) {
             char r = s.charAt(right++);
             if (map.containsKey(r)) {
                 map.put(r, map.get(r) - 1);
                 if (map.get(r) == 0) count--;
             }
-
             while (count == 0) {
                 if (right - left < minLen) {
                     minLen = right - left;
